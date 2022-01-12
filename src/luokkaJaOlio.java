65583a53ea5f670389dfec0a0e838b559d1327cc
tapi2100 <90243328+tapi2100@users.noreply.github.com>
2022-01-12 19:12:43 +0200

    commit2

--- src/luokkaJaOlio.java
+++ src/luokkaJaOlio.java
@@ -8,10 +8,28 @@ public class luokkaJaOlio {
 }
 
 class Kiekko {
+	protected String model;
 	protected String plastic;
 	protected int weight;
 	protected int speed;
 	protected int glide;
 	protected int turn;
 	protected int fade;
+	
+	public void fly()
+	{
+		System.out.println("Kiekko made of " + plastic + " flies at the speed of " + speed);
+	}
+	
+	public void printInfo()
+	{
+		System.out.println(model);
+		System.out.println(plastic);
+		System.out.println(weight);
+		System.out.println(speed);
+		System.out.println(glide);
+		System.out.println(turn);
+		System.out.println(fade);
+	}
 }
+
